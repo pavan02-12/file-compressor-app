d2a11c32d0e7063b3de6218bf0e5acd4670133fe
pavan02-12 <ypavanasish@gmail.com>
2026-03-31 23:03:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -131,11 +131,6 @@ def download_file(filename):
         return send_file(file_path, as_attachment=True)
     return "File not found.", 404
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
-
-import os
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
